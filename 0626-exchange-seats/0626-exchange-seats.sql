0557e474dfac118f93ffb1569268bbf3f60bc32d
Ved Prakash Shukla <49337175+vedpshukla@users.noreply.github.com>
2022-10-14 12:38:21 +0530

    Time: 349 ms (57.46%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,8 +1,12 @@
-SELECT ( CASE
-            WHEN id%2 != 0 AND id != counts THEN id+1
-            WHEN id%2 != 0 AND id = counts THEN id
-            ELSE id-1
-        END) AS id, student
-FROM seat, (select count(*) as counts from seat) 
-AS seat_counts
-ORDER BY id ASC;
+# SELECT ( CASE
+#             WHEN id%2 != 0 AND id != counts THEN id+1
+#             WHEN id%2 != 0 AND id = counts THEN id
+#             ELSE id-1
+#         END) AS id, student
+# FROM seat, (select count(*) as counts from seat) 
+# AS seat_counts
+# ORDER BY id ASC;
+
+SELECT ROW_NUMBER() OVER() id, student
+FROM seat
+ORDER BY IF(MOD(id, 2) = 0, id-1, id+1)
